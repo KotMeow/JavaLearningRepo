206a0bdca48d4e94ae1e60af6d0c65aab9bd6609
KotMeow <kaamil.kot@gmail.com>
2017-08-06 17:50:05 +0200

    simple jms example

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -1,14 +1,23 @@
 package com.example.learn;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+
+import javax.jms.Queue;
 
 @SpringBootApplication
-@EnableAsync
+@EnableJms
 public class LearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new ActiveMQQueue("sample");
     }
 }
--- src/main/java/com/example/learn/controller/HomeController.java
+++ src/main/java/com/example/learn/controller/HomeController.java
@@ -2,23 +2,30 @@ package com.example.learn.controller;
 
 
 import com.example.learn.model.Person;
-import com.example.learn.service.StatsService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jms.Queue;
+
 @RestController
 public class HomeController {
 
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
 
     @Autowired
-    private StatsService statsService;
+    private Queue queue;
 
 
     @PostMapping("/send")
     public Person postData(@RequestBody(required = false) Person person) throws InterruptedException {
-        statsService.calculate(person);
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        jmsMessagingTemplate.convertAndSend(this.queue, json);
         return person;
     }
 }

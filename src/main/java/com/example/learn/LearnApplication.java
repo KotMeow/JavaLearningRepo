583f6caa1c1f2c04d2dfa197f9121c3b5b2e3bcb
KotMeow <kaamil.kot@gmail.com>
2017-08-08 21:52:25 +0200

    artemis jms example with delay

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -1,12 +1,8 @@
 package com.example.learn;
 
-import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
-
-import javax.jms.Queue;
 
 @SpringBootApplication
 @EnableJms
@@ -16,8 +12,4 @@ public class LearnApplication {
         SpringApplication.run(LearnApplication.class, args);
     }
 
-    @Bean
-    public Queue queue() {
-        return new ActiveMQQueue("sample");
-    }
 }
--- src/main/java/com/example/learn/controller/HomeController.java
+++ src/main/java/com/example/learn/controller/HomeController.java
@@ -3,29 +3,30 @@ package com.example.learn.controller;
 
 import com.example.learn.model.Person;
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jms.Queue;
+import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class HomeController {
 
     @Autowired
-    private JmsMessagingTemplate jmsMessagingTemplate;
-
-    @Autowired
-    private Queue queue;
+    private JmsTemplate jmsMessagingTemplate;
 
 
     @PostMapping("/send")
     public Person postData(@RequestBody(required = false) Person person) throws InterruptedException {
         Gson gson = new Gson();
         String json = gson.toJson(person);
-        jmsMessagingTemplate.convertAndSend(this.queue, json);
+        log.info("started sending");
+        jmsMessagingTemplate.setDeliveryDelay(TimeUnit.MINUTES.toMillis(1));
+        jmsMessagingTemplate.convertAndSend("sample", json);
         return person;
     }
 }
